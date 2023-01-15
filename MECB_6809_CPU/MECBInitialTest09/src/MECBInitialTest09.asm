94f4c0083b236564d0d943808a2536bc746e7573
Greg <Greg@DigicoolThings.com>
2023-01-15 16:48:56 +1300

    Initial commit of 6809 CPU card & TMS992xA VDP card WinCupl PAL source and 6809 initial test assembly source

---
+++ MECB_6809_CPU/MECBInitialTest09/src/MECBInitialTest09.asm
@@ -0,0 +1,239 @@
+;******************************************************************************
+;	MECBInitialTest09.asm
+;
+;	A simple 6809 CPU Card test utilising just the TMS9929A VDP Card.
+;
+;	Intended as ROM based code.
+;	VDP defaults to base address $E080
+;	e.g. If IORQ based VDP allocation, then CPU card IORQ is allocated to
+;	page $E0xx and VDP address decode is based on IORQ range $80..$87
+;
+;	Author: Greg@DigicoolThings.com
+;	Date:	Jan 2023
+;
+;******************************************************************************
+Entry		EQU	$F000		; ROM code entry address
+VDP		EQU	$E080		; TMS9929A Video Display Processor address
+VDP_VRAM	EQU	VDP+0		; VDP VRAM access address
+VDP_REGISTER	EQU	VDP+1		; VDP Register access address
+;
+; Main Entry Point
+		ORG	Entry
+		LDS	#$0100	; Initialise Stack Pointer
+		LDA	#$00	; Initialise Direct Page Register
+		TFR	A,DP
+; Setup VDP
+		LDX	#GraphIModeRegs
+		BSR	tfrAllVDPRegs
+; Clear and then Setup VDP VRAM
+		JSR	vramClear
+		JSR	vramSetup
+; Loop forever through Backdrop colors 2..15
+		LDA	#1	; Initalise Backdrop color
+LoopBackdrop	INCA		; Increment Backdrop color
+		CMPA	#15	; Rotate through the 14 different Backdrop colors (2..15)
+		BLE	SkipRst	; It's not time to reset Backdrop color
+		LDA	#2	; Reset Backdrop color back to color 2
+SkipRst		LDB	#7	; Write to VDP Register 7
+		BSR	writeRegisterByteVDP
+		LDX	#1992	; Delay approximately 2 seconds
+		JSR	delayMS	; 1992 * 1.004 + 0.003 = 1999.97ms :)
+		BRA	LoopBackdrop
+;
+; *** End of Mainline, subroutines follow ***
+;
+setVramReadAddress
+; Function:	Setup VRAM Address for subsequent VRAM read
+; Parameters:	D - VRAM address
+; Returns:	-
+; Destroys:	A
+		ANDA	#$3F
+		STB	VDP_REGISTER	; Store low byte of address
+		STA	VDP_REGISTER	; Store masked high byte of address
+		RTS
+
+setVramWriteAddress
+; Function:	Setup VRAM Address for subsequent VRAM write
+; Parameters:	D - VRAM address
+; Returns:	-
+; Destroys:	A
+		ANDA	#$3F
+		ORA	#$40
+		STB	VDP_REGISTER	; Store low byte of address
+		STA	VDP_REGISTER	; Store masked high byte of address
+		RTS
+
+writeRegisterByteVDP
+; Function:	Write a data byte into a specified VDP register
+; Parameters:	A - Data Byte
+;		B - Register number
+; Returns:	-
+; Destroys:	B
+		ANDB	#$07
+		ORB	#$80
+		STA	VDP_REGISTER	; Store data byte
+		STB	VDP_REGISTER	; Store masked register number
+		RTS
+
+readStatusByteVDP
+; Function:	Read the VDP status byte
+; Note:		Routine intended for functional documentaion only
+;		i.e. Just directly inline implement: LDA VDP_REGISTER
+; Parameters:	-
+; Returns:	A = Status Byte
+; Destroys:	A
+		LDA	VDP_REGISTER
+		RTS
+
+readVramByteVDP
+; Function:	Read byte from current VRAM read address
+; Note:		Routine intended for functional documentaion only
+;		i.e. Just directly inline implement: LDA VDP_VRAM
+; Parameters:	-
+; Returns:	A = VRAM Byte read
+; Destroys:	A
+		LDA	VDP_VRAM
+		RTS
+
+writeVramByteVDP
+; Function:	Write byte to current VRAM write address
+; Note:		Routine intended for functional documentaion only
+;		i.e. Just directly inline implement: STA VDP_VRAM
+; Parameters:	A - VRAM Byte to write
+; Returns:	-
+; Destroys:	-
+		STA	VDP_VRAM
+		RTS
+
+tfrAllVDPRegs
+; Function:	Write block of 8 bytes to the VDP registers
+; Parameters:	X - Points to address of 8 byte register set
+; Returns:	-
+; Destroys:	A, B, X
+		LDB	#$80		; Initialise to register zero
+LoadRegLoop	LDA	,X+		; Load register data pointed to by X and increment X
+		STA	VDP_REGISTER	; Store data byte
+		STB	VDP_REGISTER	; Store register number
+		INCB			; Point to next register
+		CMPB	#$88		; Have we done all 8 registers?
+		BNE	LoadRegLoop	; No, do next register
+		RTS
+
+setVramBlock
+; Function:	Write specified byte to block of VRAM bytes
+; Parameters:	A - Bytes to write
+;		Y - Count of bytes to write
+; Returns:	-
+; Destroys:	A, Y
+SetVramLoop	STA	VDP_VRAM
+		LEAY	-1,Y
+		BNE	SetVramLoop
+		RTS
+
+tfrVramBlock
+; Function:	Write block of bytes to VRAM
+; Parameters:	X - Points to address of bytes to write to VRAM
+;		Y - Count of bytes to write
+; Returns:	-
+; Destroys:	A, X, Y
+TfrVramLoop	LDA	,X+		; Load VRAM data pointed to by X and increment X
+		STA	VDP_VRAM
+		LEAY	-1,Y
+		BNE	TfrVramLoop
+		RTS
+
+vramClear
+; Function:	Clear full 16KB of VDP VRAM memory
+; Parameters:	-
+; Returns:	-
+; Destroys:	A, B, Y
+		LDD	#$0000
+		JSR	setVramWriteAddress
+		CLRA	
+		LDY	#16384
+		BSR	setVramBlock
+		RTS
+
+vramSetup
+; Function:	Setup VDP VRAM Tables
+; Parameters:	-
+; Returns:	-
+; Destroys:	A, B, X, Y
+;
+; Setup Test Pattern 0 in Pattern Table
+		LDD	#$0800
+		JSR	setVramWriteAddress
+		LDX	#TestPattern
+		LDY	#8
+		BSR	tfrVramBlock
+;
+; Set Pattern 0 to be in all 768 Screen locations
+		LDD	#$1400
+		JSR	setVramWriteAddress
+		CLRA	
+		LDY	#768
+		BSR	setVramBlock
+;
+; Set Color (for Patterns 0 - 7)
+		LDD	#$2000
+		JSR	setVramWriteAddress
+		LDA	#$F4	; White (color 15) on bits, Dark Blue (color 4) off bits
+		STA	VDP_VRAM
+;
+		RTS
+
+delay1MS
+; Function:	Delay 1ms (Approximately. Actually 1.004ms at 1Mhz clock)
+; Parameters:	-
+; Returns:	-
+; Destroys:	X, Y
+		LDX	#1		; 3 Cycles
+DelayMSLoop	LDY	#123		; 4 Cycles - Assumes 1Mhz Clock
+Delay1MSLoop	LEAY	-1,Y		; 5 cycles
+		BNE	Delay1MSLoop	; 3 cycles
+		LEAX	-1,X		; 5 cycles
+		BNE	DelayMSLoop	; 3 cycles
+		RTS			; 5 cycles
+
+delayMS
+; Function:	Delay X ms (Actually X * 1.004ms + 0.003ms at 1Mhz clock)
+; Parameters:	X - Specifies desired delay in millseconds (note above)
+; Returns:	-
+; Destroys:	X, Y
+		BRA	DelayMSLoop	; 3 cycles
+
+; Default Interrupt Handler
+returnInt	RTI
+
+;
+; *** End of subroutines, data follows ***
+;
+GraphIModeRegs	FCB	$00		; Graphics I Mode
+		FCB	$C0		; Graphics I or II Mode, 8x8 Sprites, 16KB VRAM, Display Area Enabled
+		FCB	$05		; Name table start address = $1400
+		FCB	$80		; Color table start address = $2000
+		FCB	$01		; Pattern table start address = $0800
+		FCB	$20		; Sprite Attribute table start address = $1000
+		FCB	$00		; Sprite Pattern table start address = $0000
+		FCB	$01		; Transparent Text / Black Backdrop
+
+TestPattern	FCB	%00000000	; Test Pattern (Letter 'A')
+		FCB	%00011000
+		FCB	%00100100
+		FCB	%01000010
+		FCB	%01111110
+		FCB	%01000010
+		FCB	%01000010
+		FCB	%00000000
+
+; *** Interrupt Vectors ***
+		ORG	$FFF0
+		FDB	$0000		; Reserved
+		FDB	returnInt	; SWI3
+		FDB	returnInt	; SWI2
+		FDB	returnInt	; FIRQ
+		FDB	returnInt	; IRQ
+		FDB	returnInt	; SWI
+		FDB	returnInt	; NMI
+		FDB	Entry		; Reset
+		END
